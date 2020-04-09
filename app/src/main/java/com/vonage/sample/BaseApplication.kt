eb2cf43ac34fffc12e3f0cd453bf88205a4a050c
igor.wojda <igor.wojda@gmail.com>
2020-04-09 16:55:10 +0200

    Added custom application class

---
+++ app/src/main/java/com/vonage/sample/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.vonage.sample
+
+import android.app.Application
+import com.nexmo.client.NexmoClient
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Init the NexmoClient. You can retrieve NexmoClient instance latter by using NexmoClient.get()
+        NexmoClient.Builder().build(this)
+    }
+}
