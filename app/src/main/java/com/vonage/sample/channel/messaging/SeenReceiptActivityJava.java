b4b5795d6d9c4725ba26382bb9ec50684b2a319e
igor.wojda <igor.wojda@gmail.com>
2020-04-15 15:36:15 +0200

    Added SeenReceipt

---
+++ app/src/main/java/com/vonage/sample/channel/messaging/SeenReceiptActivityJava.java
@@ -0,0 +1,66 @@
+package com.vonage.sample.channel.messaging;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.nexmo.client.*;
+import com.nexmo.client.request_listener.NexmoApiError;
+import com.nexmo.client.request_listener.NexmoRequestListener;
+import timber.log.Timber;
+
+public class SeenReceiptActivityJava extends AppCompatActivity {
+
+    private NexmoMessageEventListener messageListener = new NexmoMessageEventListener() {
+
+        @Override
+        public void onTextEvent(@NonNull NexmoTextEvent textEvent) {}
+
+        @Override
+        public void onAttachmentEvent(@NonNull NexmoAttachmentEvent attachmentEvent) {}
+
+        @Override
+        public void onEventDeleted(@NonNull NexmoDeletedEvent deletedEvent) {}
+
+        @Override
+        public void onSeenReceipt(@NonNull NexmoSeenEvent seenEvent) {
+            String userName = seenEvent.getFromMember().getUser().getName();
+
+            Timber.d("Event " + seenEvent.initialEventId() + "seen by User " + userName);
+        }
+
+        @Override
+        public void onDeliveredReceipt(@NonNull NexmoDeliveredEvent deliveredEvent) {}
+
+        @Override
+        public void onTypingEvent(@NonNull NexmoTypingEvent typingEvent) {}
+    };
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        // No need for client initialization here. Client initialization is already done in BaseApplication class.
+        // new NexmoClient.Builder().build(this);
+        NexmoClient client = NexmoClient.get();
+        client.login("JWT token");
+        getConversation(client);
+
+    }
+
+    private void getConversation(NexmoClient client) {
+        client.getConversation("CONVERSATION_ID", new NexmoRequestListener<NexmoConversation>() {
+            @Override
+            public void onSuccess(@Nullable NexmoConversation conversation) {
+                Timber.d("Conversation loaded");
+                conversation.addMessageEventListener(messageListener);
+            }
+
+            @Override
+            public void onError(@NonNull NexmoApiError apiError) {
+                Timber.d("Error: Unable to load conversation %s", apiError.getMessage());
+            }
+        });
+    }
+}
