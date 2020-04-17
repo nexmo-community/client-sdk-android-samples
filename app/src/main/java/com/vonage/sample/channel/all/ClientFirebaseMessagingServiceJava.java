1de85e9f8cb39805b89e4a6301bd82ea398c64fc
igor.wojda <igor.wojda@gmail.com>
2020-04-17 13:37:56 +0200

    Add ClientFirebaseMessagingService

--- app/src/main/java/com/vonage/sample/channel/all/ClientFirebaseMessagingServiceJava.java
+++ app/src/main/java/com/vonage/sample/channel/all/ClientFirebaseMessagingServiceJava.java
@@ -3,19 +3,24 @@ package com.vonage.sample.channel.all;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.nexmo.client.NexmoCall;
 import com.nexmo.client.NexmoClient;
+import com.nexmo.client.NexmoEvent;
+import com.nexmo.client.NexmoPushEventListener;
 import com.nexmo.client.request_listener.NexmoApiError;
 import com.nexmo.client.request_listener.NexmoRequestListener;
+import timber.log.Timber;
 
 public class ClientFirebaseMessagingServiceJava extends FirebaseMessagingService {
+
+    // No need for client initialization here. Client initialization is already done in BaseApplication class.
+    // new NexmoClient.Builder().build(this);
+    private NexmoClient client = NexmoClient.get();
 
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-
-        // No need for client initialization here. Client initialization is already done in BaseApplication class.
-        // new NexmoClient.Builder().build(this);
-        NexmoClient client = NexmoClient.get();
 
         client.enablePushNotifications(token, new NexmoRequestListener<Void>() {
             @Override
@@ -24,5 +29,30 @@ public class ClientFirebaseMessagingServiceJava extends FirebaseMessagingService
             @Override
             public void onError(@NonNull NexmoApiError nexmoApiError) {}
         });
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        // determine if the message is sent from Nexmo server
+        if (NexmoClient.isNexmoPushNotification(remoteMessage.getData())) {
+            client.processNexmoPush(remoteMessage.getData(), new NexmoPushEventListener() {
+                @Override
+                public void onIncomingCall(NexmoCall call) {
+                    Timber.d("FirebaseMessage:onIncomingCall() with: " + call);
+                }
+
+                @Override
+                public void onNewEvent(NexmoEvent event) {
+                    Timber.d("FirebaseMessage:onNewEvent() with: " + event);
+                }
+
+                @Override
+                public void onError(NexmoApiError apiError) {
+                    Timber.d("FirebaseMessage:onError() with: " + apiError);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/vonage/sample/channel/all/ClientFirebaseMessagingServiceKotlin.kt
+++ app/src/main/java/com/vonage/sample/channel/all/ClientFirebaseMessagingServiceKotlin.kt
@@ -1,18 +1,23 @@
 package com.vonage.sample.channel.all
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.nexmo.client.NexmoCall
 import com.nexmo.client.NexmoClient
+import com.nexmo.client.NexmoEvent
+import com.nexmo.client.NexmoPushEventListener
 import com.nexmo.client.request_listener.NexmoApiError
 import com.nexmo.client.request_listener.NexmoRequestListener
+import timber.log.Timber
 
 class ClientFirebaseMessagingServiceKotlin : FirebaseMessagingService() {
 
+    // No need for client initialization here. Client initialization is already done in BaseApplication class.
+    // NexmoClient.Builder().build(this)
+    private val client = NexmoClient.get()
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
-        // No need for client initialization here. Client initialization is already done in BaseApplication class.
-        // NexmoClient.Builder().build(this)
-        val client = NexmoClient.get()
 
         client.enablePushNotifications(token, object : NexmoRequestListener<Void> {
             override fun onSuccess(p0: Void?) {
@@ -23,5 +28,24 @@ class ClientFirebaseMessagingServiceKotlin : FirebaseMessagingService() {
                 TODO("not implemented")
             }
         })
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // determine if the message is sent from Nexmo server
+        if (NexmoClient.isNexmoPushNotification(remoteMessage.data)) {
+            client.processNexmoPush(remoteMessage.data, object : NexmoPushEventListener {
+                override fun onIncomingCall(call: NexmoCall?) {
+                    Timber.d("FirebaseMessage:onIncomingCall() with: $call")
+                }
+
+                override fun onNewEvent(event: NexmoEvent?) {
+                    Timber.d("FirebaseMessage:onNewEvent() with: $event")
+                }
+
+                override fun onError(apiError: NexmoApiError?) {
+                    Timber.d("FirebaseMessage:onError() with: $apiError")
+                }
+            })
+        }
     }
 }
