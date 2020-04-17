43975bc635de2e3cd4783d6ab45653728e69e201
igor.wojda <igor.wojda@gmail.com>
2020-04-17 15:37:01 +0200

    Add ConfigureIceServerActivity

---
+++ app/src/main/java/com/vonage/sample/channel/all/init/ConfigureIceServerActivityJava.java
@@ -0,0 +1,19 @@
+package com.vonage.sample.channel.all.init;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.nexmo.client.NexmoClient;
+
+public class ConfigureIceServerActivityJava extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        NexmoClient client = new NexmoClient.Builder()
+                .iceServerUrls(new String[]{"STUN1_URL"})
+                .build(this);
+    }
+}
---
+++ app/src/main/java/com/vonage/sample/channel/all/init/ConfigureIceServerActivityKotlin.kt
@@ -0,0 +1,17 @@
+package com.vonage.sample.channel.all.init
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nexmo.client.NexmoClient
+
+class ConfigureIceServerActivityKotlin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        val nexmoClient = NexmoClient.Builder()
+            .iceServerUrls(arrayOf("STUN1_URL"))
+            .build(this)
+    }
+}
