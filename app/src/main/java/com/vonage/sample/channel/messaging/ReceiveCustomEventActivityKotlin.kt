ec6f814ea32f5658ad0d9030dee8446520355e61
igor.wojda <igor.wojda@gmail.com>
2020-04-20 11:33:27 +0200

    Add ReceiveCustomEvent

---
+++ app/src/main/java/com/vonage/sample/channel/messaging/ReceiveCustomEventActivityKotlin.kt
@@ -0,0 +1,41 @@
+package com.vonage.sample.channel.messaging
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nexmo.client.NexmoClient
+import com.nexmo.client.NexmoConversation
+import com.nexmo.client.NexmoCustomEventListener
+import com.nexmo.client.request_listener.NexmoApiError
+import com.nexmo.client.request_listener.NexmoRequestListener
+import timber.log.Timber
+
+class ReceiveCustomEventActivityKotlin : AppCompatActivity() {
+
+    private val customEventListener = NexmoCustomEventListener { event ->
+        Timber.d("Incoming custom event of type ${event.customType} : ${event.data}")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        // No need for client initialization here. Client initialization is already done in BaseApplication class.
+        // NexmoClient.Builder().build(this)
+        val client = NexmoClient.get()
+        getConversation(client)
+    }
+
+    private fun getConversation(client: NexmoClient) {
+        client.getConversation("CONVERSATION_ID", object : NexmoRequestListener<NexmoConversation> {
+            override fun onSuccess(conversation: NexmoConversation?) {
+                Timber.d("Conversation loaded")
+
+                conversation?.addCustomEventListener(customEventListener)
+            }
+
+            override fun onError(apiError: NexmoApiError) {
+                Timber.d("Error: Unable to load conversation ${apiError.message}")
+            }
+        })
+    }
+}
